9f69885f7c0b8c3c9115055b8c8630a117702cad
Krish Vachhani <krishvachhani19@gmail.com>
2021-09-30 19:01:26 +0530

    First Commit

--- app/src/main/java/com/example/memeshare/MainActivity.kt
+++ app/src/main/java/com/example/memeshare/MainActivity.kt
@@ -4,6 +4,7 @@ import android.content.Intent
 import android.graphics.drawable.Drawable
 import android.os.Bundle
 import android.view.View
+import android.widget.Button
 import android.widget.ImageView
 import android.widget.ProgressBar
 import android.widget.Toast
@@ -19,7 +20,10 @@ import com.bumptech.glide.request.target.Target
 class MainActivity : AppCompatActivity() {
     private lateinit var memeImage:ImageView
     private lateinit var progressBar:ProgressBar
+    private lateinit var nextButton : Button
+    private lateinit var shareButton : Button
     private var currImageUrl:String? = null
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
